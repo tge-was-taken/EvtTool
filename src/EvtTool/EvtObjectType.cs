27acc21b9390f3738043d33ec8c6ca6b172acd74
Secre-C <89033534+Secre-C@users.noreply.github.com>
2022-01-15 21:31:21 -0800

    added more EVT object types

--- src/EvtTool/EvtObjectType.cs
+++ src/EvtTool/EvtObjectType.cs
@@ -7,6 +7,10 @@ namespace EvtTool
         Camera      = 0x00000009,
         Prop        = 0x00000601,
         Particle    = 0x01000002,
-        Character   = 0x01000101
+        Character   = 0x01000101,
+        Enemy       = 0x00000301,
+        FieldObject = 0x02000701,
+        Image       = 0x00000005,
+        AltField    = 0x00000004
     }
 }
