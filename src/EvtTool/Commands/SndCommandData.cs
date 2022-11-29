1f48d8ebdcea40c6c0d04bb19184f876f77113fc
SecreC <gabirocks505@gmail.com>
2022-11-29 01:40:51 -0800

    updated Snd_ command values

--- src/EvtTool/Commands/SndCommandData.cs
+++ src/EvtTool/Commands/SndCommandData.cs
@@ -9,22 +9,24 @@ namespace EvtTool
         public int Field00 { get; set; }
         [JsonConverter(typeof(StringEnumConverter))]
         public SndMan SoundType { get; set; }
-        public int Field08 { get; set; }
-        public int Field0C { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public SndAction Action { get; set; }
+        public int Channel { get; set; }
         public int CueId { get; set; }
         public int Field14 { get; set; }
-        public int Field18 { get; set; }
+        public int FadeoutDuration { get; set; }
         public int Field1C { get; set; }
 
         internal override void Read( Command command, EndianBinaryReader reader )
         {
             Field00 = reader.ReadInt32();
             SoundType = (SndMan)reader.ReadInt32();
-            Field08 = reader.ReadInt32();
-            Field0C = reader.ReadInt32();
+            Action = (SndAction)reader.ReadInt32();
+            Channel = reader.ReadInt32();
             CueId = reader.ReadInt32();
             Field14 = reader.ReadInt32();
-            Field18 = reader.ReadInt32();
+            FadeoutDuration = reader.ReadInt32();
             Field1C = reader.ReadInt32();
         }
 
@@ -32,11 +34,11 @@ namespace EvtTool
         {
             writer.Write( Field00 );
             writer.Write( (int)SoundType);
-            writer.Write( Field08 );
-            writer.Write( Field0C );
+            writer.Write( (int)Action );
+            writer.Write( Channel );
             writer.Write( CueId );
             writer.Write( Field14 );
-            writer.Write( Field18 );
+            writer.Write( FadeoutDuration );
             writer.Write( Field1C );
         }
 
@@ -45,6 +47,12 @@ namespace EvtTool
             Bgm = 1,
             System = 2,
             Event = 3,
+        }
+
+        public enum SndAction
+        {
+            Play = 1,
+            Stop = 2,
         }
     }
 }
