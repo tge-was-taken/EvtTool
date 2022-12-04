64697f3587daee95f7764d8640e83a09dce522c7
SecreC <gabirocks505@gmail.com>
2022-12-03 23:21:19 -0800

    Labeled Flbk, MFts, and SsCp command values

--- src/EvtTool/Commands/FlbkCommandData.cs
+++ src/EvtTool/Commands/FlbkCommandData.cs
@@ -1,12 +1,15 @@
 using EvtTool.IO;
+using EvtTool.Json.Converters;
+using Newtonsoft.Json;
 
 namespace EvtTool
 {
     public sealed class FlbkCommandData : CommandData
     {
         public int Field00 { get; set; }
-        public int Field04 { get; set; }
-        public int Field08 { get; set; }
+        public short FlbkImageMajorId { get; set; }
+        public short FlbkImageMinorId { get; set; }
+        public int FlbkImageOpacity { get; set; }
         public int Field0C { get; set; }
         public float Field10 { get; set; }
         public float Field14 { get; set; }
@@ -15,7 +18,7 @@ namespace EvtTool
         public float Field20 { get; set; }
         public float Field24 { get; set; }
         public float Field28 { get; set; }
-        public int Field2C { get; set; }
+        public int ObjDrawOverImage { get; set; }
         public int Field30 { get; set; }
         public int Field34 { get; set; }
         public int Field38 { get; set; }
@@ -28,8 +31,9 @@ namespace EvtTool
         internal override void Read( Command command, EndianBinaryReader reader )
         {
             Field00 = reader.ReadInt32();
-            Field04 = reader.ReadInt32();
-            Field08 = reader.ReadInt32();
+            FlbkImageMajorId = reader.ReadInt16();
+            FlbkImageMinorId = reader.ReadInt16();
+            FlbkImageOpacity = reader.ReadInt32();
             Field0C = reader.ReadInt32();
             Field10 = reader.ReadSingle();
             Field14 = reader.ReadSingle();
@@ -38,7 +42,7 @@ namespace EvtTool
             Field20 = reader.ReadSingle();
             Field24 = reader.ReadSingle();
             Field28 = reader.ReadSingle();
-            Field2C = reader.ReadInt32();
+            ObjDrawOverImage = reader.ReadInt32();
             Field30 = reader.ReadInt32();
             Field34 = reader.ReadInt32();
             Field38 = reader.ReadInt32();
@@ -52,8 +56,9 @@ namespace EvtTool
         internal override void Write( Command command, EndianBinaryWriter writer )
         {
             writer.Write( Field00 );
-            writer.Write( Field04 );
-            writer.Write( Field08 );
+            writer.Write(FlbkImageMajorId);
+            writer.Write(FlbkImageMinorId);
+            writer.Write(FlbkImageOpacity);
             writer.Write( Field0C );
             writer.Write( Field10 );
             writer.Write( Field14 );
@@ -62,7 +67,7 @@ namespace EvtTool
             writer.Write( Field20 );
             writer.Write( Field24 );
             writer.Write( Field28 );
-            writer.Write( Field2C );
+            writer.Write( ObjDrawOverImage );
             writer.Write( Field30 );
             writer.Write( Field34 );
             writer.Write( Field38 );
--- src/EvtTool/Commands/MFtsCommandData.cs
+++ src/EvtTool/Commands/MFtsCommandData.cs
@@ -4,23 +4,34 @@ namespace EvtTool
 {
     public sealed class MFtsCommandData : CommandData
     {
-        public int Field00 { get; set; }
-        public float Field04 { get; set; }
+        public bool FootstepAreaDistortion { get; set; }
+        public bool FootstepPuddleEffect { get; set; }
+        public float FootstepDistortionStrength { get; set; }
         public int Field08 { get; set; }
         public int Field0C { get; set; }
 
         internal override void Read( Command command, EndianBinaryReader reader )
         {
-            Field00 = reader.ReadInt32();
-            Field04 = reader.ReadSingle();
+            int FootstepDistortionBitfield = reader.ReadInt32();
+            FootstepAreaDistortion = (FootstepDistortionBitfield & 2) == 2;
+            FootstepPuddleEffect = (FootstepDistortionBitfield & 4) == 4;
+            FootstepDistortionStrength = reader.ReadSingle();
             Field08 = reader.ReadInt32();
             Field0C = reader.ReadInt32();
         }
 
         internal override void Write( Command command, EndianBinaryWriter writer )
         {
-            writer.Write( Field00 );
-            writer.Write( Field04 );
+            int FootstepDistortionBitfield = 0;
+
+            if (FootstepAreaDistortion)
+                FootstepDistortionBitfield += 2;
+
+            if (FootstepPuddleEffect)
+                FootstepDistortionBitfield += 4;
+
+            writer.Write(FootstepDistortionBitfield);
+            writer.Write( FootstepDistortionStrength );
             writer.Write( Field08 );
             writer.Write( Field0C );
         }
