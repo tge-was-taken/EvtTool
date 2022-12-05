0c8c4e81d83e87ea605fa1e8cc850882cc78bc41
SecreC <gabirocks505@gmail.com>
2022-12-04 17:02:52 -0800

    used proper bitfields for mfts

--- src/EvtTool/Commands/MFtsCommandData.cs
+++ src/EvtTool/Commands/MFtsCommandData.cs
@@ -4,17 +4,17 @@ namespace EvtTool
 {
     public sealed class MFtsCommandData : CommandData
     {
-        public bool FootstepAreaDistortion { get; set; }
-        public bool FootstepPuddleEffect { get; set; }
+        public FootstepDistortion FootstepDistortionBitfield { get; set; }
         public float FootstepDistortionStrength { get; set; }
         public int Field08 { get; set; }
         public int Field0C { get; set; }
 
         internal override void Read( Command command, EndianBinaryReader reader )
         {
-            int FootstepDistortionBitfield = reader.ReadInt32();
-            FootstepAreaDistortion = (FootstepDistortionBitfield & 2) == 2;
-            FootstepPuddleEffect = (FootstepDistortionBitfield & 4) == 4;
+            FootstepDistortionBitfield = new FootstepDistortion
+            {
+                data = reader.ReadInt32()
+            };
             FootstepDistortionStrength = reader.ReadSingle();
             Field08 = reader.ReadInt32();
             Field0C = reader.ReadInt32();
@@ -22,18 +22,33 @@ namespace EvtTool
 
         internal override void Write( Command command, EndianBinaryWriter writer )
         {
-            int FootstepDistortionBitfield = 0;
-
-            if (FootstepAreaDistortion)
-                FootstepDistortionBitfield += 2;
-
-            if (FootstepPuddleEffect)
-                FootstepDistortionBitfield += 4;
-
-            writer.Write(FootstepDistortionBitfield);
+            writer.Write( FootstepDistortionBitfield.data );
             writer.Write( FootstepDistortionStrength );
             writer.Write( Field08 );
             writer.Write( Field0C );
+        }
+
+        public class FootstepDistortion
+        {
+            internal int data { get;  set; }
+
+            public bool dummyBit
+            {
+                get { return (data & 1) == 1; }
+                set { data += (value ? 1 : 0); }
+            }
+
+            public bool AreaDistortion
+            {
+                get { return (data & 2) == 2; }
+                set { data += (value ? 1 : 0) << 1; }
+            }
+
+            public bool PuddleEffect
+            {
+                get { return (data & 4) == 4; }
+                set { data += (value ? 1 : 0) << 2; }
+            }
         }
     }
 }
